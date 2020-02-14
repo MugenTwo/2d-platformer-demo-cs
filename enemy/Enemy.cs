db21cdea1858748a5d1713926b6f78c36080994f
MugenTwo <johncarlo.purihin@gmail.com>
2020-02-14 23:50:10 +0100

    Enemy interactions implemented

--- enemy/Enemy.cs
+++ enemy/Enemy.cs
@@ -7,6 +7,8 @@ public class Enemy : RigidBody2D
     private static readonly int WALK_SPEED = 50;
     private static readonly int STATE_WALKING = 0;
     private static readonly int STATE_DYING = 1;
+    private static readonly float ANGULAR_VELOCITY_CONSTANT = 33.0f;
+    private static readonly float FRICTION_CONSTANT = 1.0f;
 
     private int state;
     private int direction;
@@ -43,13 +45,104 @@ public class Enemy : RigidBody2D
     public void BulletCollider(Bullet bullet, Physics2DDirectBodyState bodyState, Vector2 contactLocalNormal)
     {
         Mode = ModeEnum.Rigid;
-        state = STATE_DYING;
+        this.state = STATE_DYING;
 
-        AngularVelocity = Mathf.Sign(contactLocalNormal.x) * 33.0f;
-        Friction = 1.0f;
+        bodyState.AngularVelocity = Mathf.Sign(contactLocalNormal.x) * ANGULAR_VELOCITY_CONSTANT;
+        Friction = FRICTION_CONSTANT;
         bullet.Disable();
+
         AudioStreamPlayer2D soundHit = GetNode("SoundHit") as AudioStreamPlayer2D;
         soundHit.Play();
+    }
+
+    public override void _IntegrateForces(Physics2DDirectBodyState bodyState)
+    {
+        Vector2 linearVelocity = LinearVelocity;
+        String newAnimation = this.animation;
+
+        if (state == STATE_DYING)
+        {
+            newAnimation = "explode";
+        }
+        else if (state == STATE_WALKING)
+        {
+            newAnimation = "walk";
+            float wallSide = 0.0f;
+
+            for (int i = 0; i < bodyState.GetContactCount(); i++)
+            {
+                Godot.Object contactColliderObject = bodyState.GetContactColliderObject(i);
+                Vector2 contactLocalNormal = bodyState.GetContactLocalNormal(i);
+
+                if (contactColliderObject != null)
+                {
+                    if (contactColliderObject is Bullet)
+                    {
+                        Bullet bullet = contactColliderObject as Bullet;
+                        CallDeferred("BulletCollider", contactColliderObject, bodyState, contactLocalNormal);
+                        break;
+                    }
+                }
+
+                if (contactLocalNormal.x > 0.9f)
+                {
+                    wallSide = 1.0f;
+                }
+                else
+                {
+                    wallSide = -1.0f;
+                }
+            }
+
+
+            int correctDirection = findCorrectDirection(wallSide);
+
+            if (this.direction != correctDirection)
+            {
+                this.direction = correctDirection;
+
+                Sprite sprite = GetNode("Sprite") as Sprite;
+                Vector2 scale = sprite.Scale;
+                scale.x = -this.direction;
+                sprite.Scale = scale;
+            }
+
+            linearVelocity.x = this.direction * WALK_SPEED;
+        }
+
+        bodyState.LinearVelocity = linearVelocity;
+
+
+        if (animation != newAnimation)
+        {
+            UpdateAnimation(newAnimation);
+        }
+    }
+
+    private int findCorrectDirection(float wallSide)
+    {
+        if (wallSide != 0 && wallSide != direction)
+        {
+            return -this.direction;
+        }
+
+        if (direction < 0 && !rayCastLeft.IsColliding() && rayCastRight.IsColliding())
+        {
+            return -this.direction;
+        }
+        else if (direction > 0 && !rayCastRight.IsColliding() && rayCastLeft.IsColliding())
+        {
+            return -this.direction;
+        }
+
+        return this.direction;
+    }
+    
+    private void UpdateAnimation(String newAnimation)
+    {
+        this.animation = newAnimation;
+        AnimationPlayer animationPlayer = GetNode("Anim") as AnimationPlayer;
+        animationPlayer.Play(animation);
     }
 
 }
